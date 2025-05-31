ca60354ba8cc82ed7f26624c38fcbca8423cf088
CodeArcadia <ericmcgray184@gmail.com>
2025-05-31 21:59:02 +0500

    🔴 Delete some codes

--- src/components/Section.tsx
+++ src/components/Section.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+ import React, { useState, useEffect } from 'react';
 import { FaBolt, FaCalculator, FaChartBar, FaDollarSign, FaArrowRight } from 'react-icons/fa';
 
 const Section = () => {
